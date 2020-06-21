ddf602b72ec025eea096631f6f8ac9dd87af45c4
Sohaib Ashraf <sohaib.ashraf94@gmail.com>
2020-06-21 21:53:00 +0100

    Updates

---
+++ precache-manifest.6ffd101beeb79d7d0b69fbb0e95100c2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f21b1bc66892d23b715e7fb942bde73",
+    "url": "/cv-website/index.html"
+  },
+  {
+    "revision": "1fd3e6aa36366be00e3d",
+    "url": "/cv-website/static/css/main.1ef7873e.chunk.css"
+  },
+  {
+    "revision": "8546315fbbb80e9637b5",
+    "url": "/cv-website/static/js/2.ba0d1150.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-website/static/js/2.ba0d1150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd3e6aa36366be00e3d",
+    "url": "/cv-website/static/js/main.c87e5cc5.chunk.js"
+  },
+  {
+    "revision": "1bb5ca568d8e9cdcc265",
+    "url": "/cv-website/static/js/runtime-main.efe8a0b7.js"
+  },
+  {
+    "revision": "b43c579f813f90410a76a587e8f27b1a",
+    "url": "/cv-website/static/media/Kings_College_London_logo.b43c579f.svg"
+  },
+  {
+    "revision": "540b160a9d14381bb11fb40af8df8a58",
+    "url": "/cv-website/static/media/apadmi-logo.540b160a.svg"
+  },
+  {
+    "revision": "fb10d6296584daeb82dd208c046d583e",
+    "url": "/cv-website/static/media/deloitte_logo.fb10d629.png"
+  },
+  {
+    "revision": "01c6d62c546a43f71abc89ef89e6376b",
+    "url": "/cv-website/static/media/ibm_logo.01c6d62c.png"
+  },
+  {
+    "revision": "4be8e7708aaf2326e158c69890bcc775",
+    "url": "/cv-website/static/media/profile.4be8e770.png"
+  }
+]);
